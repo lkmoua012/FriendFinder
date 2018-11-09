aa412049af2c344176583b9362900bb0295f7134
larry.moua <lkmoua@gmail.com>
2018-11-09 14:44:24 -0800

    html complete

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendArray = [
+    {
+        name: "Leta Henry",
+        photo: "https://randomuser.me/api/portraits/women/72.jpg",
+        scores: [
+            5,
+            2,
+            2,
+            3,
+            3,
+            5,
+            5,
+            2,
+            5,
+            2
+        ]
+    },
+    {
+        name: "Patsy Evans",
+        photo: "https://randomuser.me/api/portraits/women/34.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            5,
+            3,
+            2,
+            2,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Ricardo Gray",
+        photo: "https://randomuser.me/api/portraits/men/19.jpg",
+        scores: [
+            3,
+            4,
+            3,
+            5,
+            2,
+            2,
+            3,
+            3,
+            5,
+            5
+        ]
+    }
+];
+
+module.exports = friendArray;
