6533f195d62ccb4e25f2bb834ff8d31bd75982be
larry.moua <lkmoua@gmail.com>
2018-11-14 13:50:19 -0800

    added post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,13 @@
 var friendsArray = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsArray);
     })
+
+    app.post("/api/friends", function (req, res) {
+        friendsArray.push(req.body);
+    });
 
 }
